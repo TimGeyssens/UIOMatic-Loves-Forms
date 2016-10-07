be16d09df7fcc161ba00490e2663a2b4dfbe2a13
TimGeyssens <tg@umbraco.com>
2016-10-07 22:30:59 +0200

    Adds vs solution

---
+++ src/UIOMaticLovesForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UIOMaticLovesForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UIOMaticLovesForms")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a1f099f4-6466-4e75-82f7-b23762d38cb1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/UIOMaticLovesForms/Providers/UIOMaticPrevalueSource.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using UIOMatic.Controllers;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+using Umbraco.Forms.Core;
+
+namespace UIOMaticLovesForms.Providers
+{
+    public class UIOMaticPrevalueSource : FieldPreValueSourceType
+    {
+        private List<PreValue> prevalues;
+
+        public UIOMaticPrevalueSource()
+        {
+            Id = new Guid("489921FA-DAD4-47A6-9682-55F137631867");
+            Name = "UI-O-Matic poco";
+            Description = "Fetches prevalues from an UI-O-Matic poco";
+
+            this.prevalues = new List<PreValue>();
+        }
+
+
+        [Umbraco.Forms.Core.Attributes.Setting("Type Of Object", description = "Type of the poco", view = "~/App_Plugins/UIOMaticLovesForms/FieldSettingTypes/Pickers.TypeOfObject.html")]
+        public string TypeOfObject { get; set; }
+
+        [Umbraco.Forms.Core.Attributes.Setting("Sort Column", description = "Column to sort on")]
+        public string SortColumn { get; set; }
+
+        [Umbraco.Forms.Core.Attributes.Setting("Sort Order", description = "Sort order", view = "dropdownlist", prevalues = "Ascending,Descending")]
+        public string SortOrder{ get; set; }
+
+        public override List<PreValue> GetPreValues(Field field, Form form)
+        {
+            var currentType = Type.GetType(TypeOfObject);
+
+            var primaryKeyColum = "id";
+
+            var primKeyAttri = currentType.GetCustomAttributes().Where(x => x.GetType() == typeof(PrimaryKeyAttribute));
+            if (primKeyAttri.Any())
+                primaryKeyColum = ((PrimaryKeyAttribute)primKeyAttri.First()).Value;
+
+            foreach (var property in currentType.GetProperties())
+            {
+                var keyAttri = property.GetCustomAttributes().Where(x => x.GetType() == typeof(PrimaryKeyColumnAttribute));
+                if (keyAttri.Any())
+                    primaryKeyColum = property.Name;
+            }
+
+            var controller = new PetaPocoObjectController();
+
+            int sortOrderCounter = 0;
+
+            foreach (var prevalue in controller.GetAll(TypeOfObject,SortColumn,SortOrder == "Ascending" ? "asc" : "desc"))
+            {
+                PreValue pv = new PreValue();
+                pv.Id = currentType.GetProperty(primaryKeyColum).GetValue(prevalue, null);
+                pv.Value = prevalue.ToString();
+                pv.SortOrder = sortOrderCounter;
+
+                //Add it to the list
+                this.prevalues.Add(pv);
+
+                //Increase counter
+                sortOrderCounter++;
+            }
+
+            return prevalues;
+        }
+
+        public override List<Exception> ValidateSettings()
+        {
+            var exceptions = new List<Exception>();
+
+            if (string.IsNullOrEmpty(TypeOfObject))
+            {
+                exceptions.Add(new Exception("'TypeOfObject' setting has not been set"));
+            }
+
+            return exceptions;
+        }
+    }
+}
