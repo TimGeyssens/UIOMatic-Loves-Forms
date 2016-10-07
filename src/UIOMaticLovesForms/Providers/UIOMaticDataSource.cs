52fcb64cc2bd73e7fb12d3f98e47f562fa4db4f7
TimGeyssens <tg@umbraco.com>
2016-10-07 23:58:01 +0200

    Adds first version of ui-o-matic datasourcetype

---
+++ src/UIOMaticLovesForms/Providers/UIOMaticDataSource.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using UIOMatic.Controllers;
+using Umbraco.Forms.Core;
+using Umbraco.Forms.Data.Storage;
+
+namespace UIOMaticLovesForms.Providers
+{
+    public class UIOMaticDataSource : FormDataSourceType
+    {
+
+        public UIOMaticDataSource()
+        {
+            this.Id = new Guid("787C2A7C-92D5-4098-B9FB-8677CD9EBAA7");
+            this.Name = "UIOMatic Poco";
+            this.Description = "Sends records to a db table";
+            this.SupportsGetRecords = false;
+            this.SupportsPrevalues = false;
+        }
+
+        [Umbraco.Forms.Core.Attributes.Setting("Type Of Object", description = "Type of the poco", view = "~/App_Plugins/UIOMaticLovesForms/FieldSettingTypes/Pickers.TypeOfObject.html")]
+        public string TypeOfObject { get; set; }
+
+        public override void Dispose()
+        {
+           
+        }
+
+        public override Dictionary<object, FormDataSourceField> GetAvailableFields()
+        {
+
+           var fields = new Dictionary<object, FormDataSourceField>();
+
+            var pc = new PetaPocoObjectController();
+
+            foreach(var prop in pc.GetAllProperties(TypeOfObject))
+            {
+                FormDataSourceField fdsf = FormDataSourceField.Create();
+                fdsf.AllowNulls = false;
+                fdsf.AutoIncrement = false;
+                fdsf.Type = prop.GetType();
+                fdsf.Name = prop.Name;
+                fdsf.Key = prop.Key;
+                fdsf.IsMandatory = false;
+
+                fields.Add(fdsf.Key, fdsf);
+            }
+
+            return fields;
+        }
+
+        public override Dictionary<object, FormDataSourceField> GetMappedFields()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Dictionary<object, string> GetPrevalues(Field field, Form form)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Record> GetRecords(Form form, int page, int maxItems, object sortByField, Sorting Order)
+        {
+            return new List<Record>();
+        }
+
+        public override Record InsertRecord(Record record)
+        {
+            var fieldMappings = record.GetForm().DataSource.Mappings;
+
+            var inst = new ExpandoObject() as IDictionary<string, Object>;
+
+            foreach (FormDataSourceMapping map in fieldMappings)
+            {
+
+                object val = new object();
+
+                if (string.IsNullOrEmpty(map.DefaultValue))
+                {
+
+                    foreach (RecordField rf in record.RecordFields.Values)
+                    {
+
+                        if (rf.Field.DataSourceFieldKey.ToString() == map.DataFieldKey.ToString())
+                        {
+                            val = Convert.ChangeType(rf.ValuesAsString(), rf.Field.FieldType.GetDataType());
+                        }
+
+                    }
+
+                }
+                else
+                    val = map.DefaultValue;
+
+                inst.Add(map.DataFieldKey.ToString(), val);
+
+
+            }
+
+            var pc = new PetaPocoObjectController();
+            var x = new ExpandoObject() as IDictionary<string, Object>;
+            x.Add("typeOfObject", TypeOfObject);
+            x.Add("objectToCreate", inst);
+            pc.PostCreate((ExpandoObject)x);
+            return record;
+        }
+
+        public override List<Exception> ValidateSettings()
+        {
+            var exceptions = new List<Exception>();
+
+            if (string.IsNullOrEmpty(TypeOfObject))
+            {
+                exceptions.Add(new Exception("'TypeOfObject' setting has not been set"));
+            }
+
+            return exceptions;
+        }
+    }
+}
