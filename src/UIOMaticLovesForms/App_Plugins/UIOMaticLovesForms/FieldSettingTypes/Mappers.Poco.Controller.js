959e1391f54a64d29ab9b0fcad0a602c7f349153
TimGeyssens <tg@umbraco.com>
2016-10-10 09:08:33 +0200

    Adds Poco mapper field setting type

---
+++ src/UIOMaticLovesForms/App_Plugins/UIOMaticLovesForms/FieldSettingTypes/Mappers.Poco.Controller.js
@@ -0,0 +1,37 @@
+﻿angular.module("umbraco").controller("UIOMaticLovesForms.SettingTypes.Mappers.PocoController",
+	function ($scope, $routeParams, pickerResource, uioMaticObjectResource, $http) {
+
+	    if (!$scope.setting.value) {
+	       
+	    } else {
+	        var value = JSON.parse($scope.setting.value);
+	        $scope.typeOfObject = value.typeOfObject;
+	        $scope.properties = value.properties;
+	    }
+
+	     $http.get(Umbraco.Sys.ServerVariables.uioMatic.pecBaseUrl + "GetAllTypes").then(function (response) {
+	        $scope.types = response.data;
+
+	    });
+
+	    pickerResource.getAllFields($routeParams.id).then(function (response) {
+	        $scope.fields = response.data;
+	    });
+
+	    $scope.setTypeOfObject = function () {
+
+	        uioMaticObjectResource.getAllProperties($scope.typeOfObject).then(function (response) {
+	            $scope.properties = response.data;
+	            console.log($scope.properties);
+	        });
+	    };
+
+	    $scope.setValue = function() {
+	       
+	        var val = {};
+	        val.typeOfObject = $scope.typeOfObject;
+	        val.properties = $scope.properties;
+
+	        $scope.setting.value = JSON.stringify(val);
+	    };
+	});
